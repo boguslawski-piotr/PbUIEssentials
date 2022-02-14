26fe44872a63c9528c233976cbc6900a2150aacd
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2022-02-14 17:10:17 +0100

    Remove unnecessary code.

--- Sources/PbUIEssentials/SwiftUI/MenuButton.swift
+++ Sources/PbUIEssentials/SwiftUI/MenuButton.swift
@@ -2,16 +2,12 @@ import SwiftUI
 
 public struct MenuButton<Content>: View where Content: View {
     public var systemImage: String
-    public var imageColor: Color?
-    public var imageScale: Image.Scale
     public var title: String
     
     @ViewBuilder public var content: () -> Content
 
-    public init(systemImage: String, imageColor: Color? = .accentColor, imageScale: Image.Scale = .large, title: String = "", content: @escaping () -> Content) {
+    public init(systemImage: String, title: String = "", content: @escaping () -> Content) {
         self.systemImage = systemImage
-        self.imageColor = imageColor
-        self.imageScale = imageScale
         self.title = title
         self.content = content
     }
@@ -21,8 +17,6 @@ public struct MenuButton<Content>: View where Content: View {
             content()
         } label: {
             Label(title, systemImage: systemImage)
-                .imageScale(imageScale)
-                .foregroundColor(imageColor)
         }
     }
 }
