3b99a260f4e77c8590f0690495c27fc1fa3dfb49
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2022-02-14 10:43:19 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PbUIEssentials
+
+A description of this package.
---
+++ Sources/PbUIEssentials/PbUIEssentials.swift
@@ -0,0 +1,6 @@
+public struct PbUIEssentials {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
