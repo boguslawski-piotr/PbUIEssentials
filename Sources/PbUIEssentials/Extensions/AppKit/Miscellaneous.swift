09b45a29ad26c68f171552ca8302cb8604703287
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2022-02-16 21:31:42 +0100

    NSOpenPanel.selectDirectory.

--- Sources/PbUIEssentials/Extensions/AppKit/Miscellaneous.swift
+++ Sources/PbUIEssentials/Extensions/AppKit/Miscellaneous.swift
@@ -1,6 +1,7 @@
 #if os(macOS)
 
 import AppKit
+import PbEssentials
 
 /// Removes background from TextEditor.
 extension NSTextView {
@@ -21,6 +22,31 @@ extension NSTableView {
         if let esv = enclosingScrollView {
             esv.drawsBackground = false
         }
+    }
+}
+
+extension NSOpenPanel {
+    private class OpenPanelDelegate: NSObject, NSOpenSavePanelDelegate {
+        func panel(_ sender: Any, shouldEnable url: URL) -> Bool {
+            return true
+        }
+    }
+    
+    private static var openPanelDelegate = OpenPanelDelegate()
+    
+    public static func selectDirectory(_ title: String, showsHiddenFiles: Bool = true) -> URL? {
+        let openPanel = NSOpenPanel()
+        
+        openPanel.title = title
+        openPanel.canChooseFiles = false
+        openPanel.canCreateDirectories = true
+        openPanel.canChooseDirectories = true
+        openPanel.showsHiddenFiles = showsHiddenFiles
+        openPanel.allowsMultipleSelection = false
+        openPanel.delegate = openPanelDelegate
+        
+        let result = openPanel.runModal()
+        return result == .OK ? openPanel.url : nil
     }
 }
 
